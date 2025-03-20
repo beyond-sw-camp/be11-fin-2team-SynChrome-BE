ab834efec430c3e7bd94787eba764da2d7a1517b
JungJunHwan <qoqkd0221@naver.com>
2025-03-20 16:08:55 +0900

    init upload

---
+++ collabcontent/src/main/java/com/Synchrome/collabcontent/CollabcontentApplication.java
@@ -0,0 +1,12 @@
+package com.Synchrome.collabcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabcontentApplication.class, args);
+	}
+}
---
+++ eureka/src/test/java/com/Synchrome/collabcontent/CollabcontentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Synchrome.collabcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabcontentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
