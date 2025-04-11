3a8082ca4a81e077d92638a758cf16fb7b7e0ccf
DESKTOP-SR275TT\Playdata <99dlatjdwo@gmail.com>
2025-04-11 12:01:53 +0900

    워크스페이스 호출 수정

--- workspace/src/main/java/com/Synchrome/workspace/space/repository/WorkSpaceParticipantRepository.java
+++ workspace/src/main/java/com/Synchrome/workspace/space/repository/WorkSpaceParticipantRepository.java
@@ -1,11 +1,15 @@
 package com.Synchrome.workspace.space.repository;
 
+import com.Synchrome.workspace.space.domain.ENUM.Del;
 import com.Synchrome.workspace.space.domain.WorkSpace;
 import com.Synchrome.workspace.space.domain.WorkSpaceParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorkSpaceParticipantRepository extends JpaRepository<WorkSpaceParticipant,Long> {
     boolean existsByUserIdAndWorkSpace(Long userId, WorkSpace workSpace);
+    List<WorkSpaceParticipant> findByUserIdAndDel(Long userId, Del del);
 }
