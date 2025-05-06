571a2c656f9302b2484c79c0f54347334c5f2713
unknown <dksqjq4851>
2025-05-07 05:00:07 +0900

    검색

--- collabcontent/src/main/java/com/Synchrome/collabcontent/opensearch/controller/SearchController.java
+++ collabcontent/src/main/java/com/Synchrome/collabcontent/opensearch/controller/SearchController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -40,5 +41,22 @@ public class SearchController {
         return Arrays.stream(response.getHits().getHits())
                 .map(SearchHit::getSourceAsMap)
                 .toList();
+    }
+
+    @GetMapping("/keyword")
+    public List<Map<String, Object>> searchMessages(@RequestParam String keyword) throws IOException {
+        SearchRequest request = new SearchRequest("chat_messages");
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.queryStringQuery("*" + keyword + "*").field("content"));
+        request.source(sourceBuilder);
+
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (SearchHit hit : response.getHits()) {
+            result.add(hit.getSourceAsMap());
+        }
+        return result;
     }
 }
